28b7fade74e3e7fc2aa35461048028ef5d9984ae
1hyung <ryu1hyung@naver.com>
2024-05-16 16:05:02 +0900

    Study: 숙련 1-11. 예외처리 및 트랜잭션 처리하기

--- src/main/kotlin/com/teamsparta/courseregistration/domain/course/controller/CourseController.kt
+++ src/main/kotlin/com/teamsparta/courseregistration/domain/course/controller/CourseController.kt
@@ -3,38 +3,66 @@ package com.teamsparta.courseregistration.domain.course.controller
 import com.teamsparta.courseregistration.domain.course.dto.CourseResponse
 import com.teamsparta.courseregistration.domain.course.dto.CreateCourseRequest
 import com.teamsparta.courseregistration.domain.course.dto.UpdateCourseRequest
+import com.teamsparta.courseregistration.domain.course.service.CourseService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/courses") //courses 관련된거 CourseController가 담당하게 된다.
+
+@RequestMapping("/courses")//courses 관련된거 CourseController가 담당하게 된다.
 //애를 빈으로 등록해줘!, 데이터만 리턴하기 때문에 Controller 어노테이션도 쓸 수 있지만
-@RestController// 이러면 controller 자체가 빈으로 등록된다. 이 컨트롤러가 어떤 URL을 서빙을 하느냐를 알려줘야함
-class CourseController {
-    // 이것을 알랴주는 것이 Handler Mapping 알려주는 것이다.
-//controller 상위에 RequestMapping이 붙어있으면 하위에 있는 메소드 같은 경우에는 @RequestMapping("/courses 뒤로 온다
+@RestController // 이러면 controller 자체가 빈으로 등록된다. 이 컨트롤러가 어떤 URL을 서빙을 하느냐를 알려줘야함
+class CourseController(  // 이것을 알랴주는 것이 Handler Mapping 알려주는 것이다.
+    //controller 상위에 RequestMapping이 붙어있으면 하위에 있는 메소드 같은 경우에는 @RequestMapping("/courses 뒤로 온다
+    private val courseService: CourseService
+) {
+
     @GetMapping()
     fun getCourseList(): ResponseEntity<List<CourseResponse>> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(courseService.getAllCourseList())
     }
 
     @GetMapping("/{courseId}")
     fun getCourse(@PathVariable courseId: Long): ResponseEntity<CourseResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(courseService.getCourseById(courseId))
     }
 
     @PostMapping
     fun createCourse(@RequestBody createCourseRequest: CreateCourseRequest): ResponseEntity<CourseResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(courseService.createCourse(createCourseRequest))
     }
 
-    //생성을 하면 수정을 할 수 있어야 함
     @PutMapping("/{courseId}")
-    fun updateCourse(@PathVariable courseId: Long, @RequestBody updateCourseRequest: UpdateCourseRequest): ResponseEntity<CourseResponse> {
-        TODO()
+    fun updateCourse(
+        @PathVariable courseId: Long,
+        @RequestBody updateCourseRequest: UpdateCourseRequest
+    ): ResponseEntity<CourseResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(courseService.updateCourse(courseId, updateCourseRequest))
     }
 
     @DeleteMapping("/{courseId}")
-    fun deleteCourse(@PathVariable courseId: Long) {
-        TODO()
+    fun deleteCourse(@PathVariable courseId: Long): ResponseEntity<Unit> {
+        courseService.deleteCourse(courseId)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
     }
+
 }
+
+// CourseController.kt
