f8a83289ab97bdc7e2a6b7d3fdaaed4ca0304835
1hyung <ryu1hyung@naver.com>
2024-05-17 12:49:48 +0900

    Study: 숙련 1-13. 다른 Service 작성 및 Controller 연결하기

--- src/main/kotlin/com/teamsparta/courseregistration/domain/lecture/controller/LectureController.kt
+++ src/main/kotlin/com/teamsparta/courseregistration/domain/lecture/controller/LectureController.kt
@@ -1,32 +1,49 @@
 package com.teamsparta.courseregistration.domain.lecture.controller
 
+import com.teamsparta.courseregistration.domain.course.service.CourseService
 import com.teamsparta.courseregistration.domain.lecture.dto.AddLectureRequest
 import com.teamsparta.courseregistration.domain.lecture.dto.LectureResponse
 import com.teamsparta.courseregistration.domain.lecture.dto.UpdateLectureRequest
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @RequestMapping("/courses/{courseId}/lectures")
 @RestController
-class LectureController {
+class LectureController(
+    private val courseService: CourseService
+) {
 
     @GetMapping
     fun getLectureList(@PathVariable courseId: Long): ResponseEntity<List<LectureResponse>> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(courseService.getLectureList(courseId))
     }
 
     @GetMapping("/{lectureId}")
     fun getLecture(@PathVariable courseId: Long, @PathVariable lectureId: Long): ResponseEntity<LectureResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(courseService.getLecture(courseId, lectureId))
     }
-
 
     @PostMapping()
     fun addLecture(
         @PathVariable courseId: Long,
         @RequestBody addLectureRequest: AddLectureRequest
     ): ResponseEntity<LectureResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(courseService.addLecture(courseId, addLectureRequest))
     }
 
     @PutMapping("/{lectureId}")
@@ -35,12 +52,17 @@ class LectureController {
         @PathVariable lectureId: Long,
         @RequestBody updateLectureRequest: UpdateLectureRequest
     ): ResponseEntity<LectureResponse> {
-        TODO("not implemented")
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(courseService.updateLecture(courseId, lectureId, updateLectureRequest))
     }
 
     @DeleteMapping("/{lectureId}")
     fun removeLecture(@PathVariable courseId: Long, @PathVariable lectureId: Long): ResponseEntity<Unit> {
-        TODO()
+        courseService.removeLecture(courseId, lectureId)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
     }
 
 }
