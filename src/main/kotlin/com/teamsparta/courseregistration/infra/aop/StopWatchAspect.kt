1771a4dfb78b833ca00c63f9468f6bc9325f367e
1hyung <ryu1hyung@naver.com>
2024-06-05 10:18:57 +0900

    feat: import org.springframework.util.StopWatch

--- src/main/kotlin/com/teamsparta/courseregistration/infra/aop/StopWatchAspect.kt
+++ src/main/kotlin/com/teamsparta/courseregistration/infra/aop/StopWatchAspect.kt
@@ -5,6 +5,7 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import org.springframework.util.StopWatch
 
 @Aspect
 @Component
